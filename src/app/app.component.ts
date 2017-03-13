398b7544933a3c6acfcedc507c6f6da8f3f8da1f
jorge52an <jorgesolavila@gmail.com>
2017-03-13 13:14:42 -0500

    Chore: add initial configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from "@angular/core";
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'app works!';
+@Component(
+{
+	selector: "app-root",
+	templateUrl: "./app.component.html",
+	styleUrls: ["./app.component.scss"],
+	encapsulation: ViewEncapsulation.None
+} )
+
+export class AppComponent
+{
+	title = "Angular Styles";
 }
